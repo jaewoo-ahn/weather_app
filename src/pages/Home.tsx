c87e759b6f43f95985c82726bc94a96120a2a59b
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2025-02-14 21:36:19 +0900

    feat :current location

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,45 @@
-import { useEffect } from "react";
-import { fetchWeatherData } from "../hooks/useWeather";
+import { useEffect, useState } from "react";
+import { fetchWeatherData } from "../api/useWeather";
 
 const Home = () => {
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      let lat = position.coords.latitude;
-      let lon = position.coords.longitude;
+  const [lat, setLat] = useState<number>(0);
+  const [lon, setLon] = useState<number>(0);
 
-      fetchWeatherData(lat, lon);
-    });
-  }, []);
+  const currentLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lon = position.coords.longitude;
+          setLat(lat);
+          setLon(lon);
+        },
+        (error) => {
+          console.error("Error getting location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  };
+
+  useEffect(() => {
+    currentLocation();
+    fetchWeatherData(lat, lon);
+  }, [lat, lon]);
 
   return (
     <div>
-      <section>현재날씨</section>
+      <section>
+        현재날씨
+        <button
+          onClick={() => {
+            currentLocation();
+          }}
+        >
+          현재위치
+        </button>
+      </section>
       <section>시간별 날씨</section>
       <section>주간날씨</section>
     </div>
